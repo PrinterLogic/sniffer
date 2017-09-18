5e1f94739431b46917ff91d5440ec92e95e2f9a4
Josh Harrison <josh@picklewagon.com>
2017-09-17 23:21:04 -0600

    Figure out line numbers for modified files

--- src/ChangedFilesFinder.php
+++ src/ChangedFilesFinder.php
@@ -3,15 +3,15 @@ namespace Ayeo\Sniffer;
 
 class ChangedFilesFinder
 {
-	private $basePath;
+    private $basePath;
 
-	public function __construct($basePath)
-	{
-		$this->basePath = $basePath;
-	}
+    public function __construct($basePath)
+    {
+        $this->basePath = $basePath;
+    }
 
-	public function findAll($commitSHA)
-	{
+    public function findAll($commitSHA)
+    {
         $commands = [
             'added' => 'cd %s && git ls-files -o  --exclude-standard --full-name',
             'modified' => 'cd %s && git ls-files -m',
@@ -20,59 +20,73 @@ class ChangedFilesFinder
         $changedFilesList = [];
         foreach ($commands as $type => $command) {
             $parsedCommand = sprintf($command, $this->basePath);
-            $changedFiles = array_filter(explode("\n", `$parsedCommand`), function($item) { return strlen($item) > 0; });
+            $changedFiles = array_filter(explode("\n", `$parsedCommand`), function($item) {
+                return strlen($item) > 0;
+            });
+
             foreach ($changedFiles as $file) {
-                $lines = 0;
+                dump($file);
+                $lineNumbers = [];
+
                 if ('added' == $type) {
                     $lines = count(file($this->basePath . $file));
+
+                    for ($i = 1; $i <= $lines; $i++) {
+                        $lineNumbers[] = (int) $i;
+                    }
                 } else if ('modified' == $type) {
-                    $lines = "cd %s && git blame test.php | grep -n '^0\{8\} ' | cut -f1 -d:";
-                }
-                //dump($lines);
+                    $lineCommand = sprintf("cd %s && git blame %s | grep -n '^0\{8\} ' | cut -f1 -d:",
+                        $this->basePath, $file);
+                    $lines = array_filter(explode("\n", `$lineCommand`), function($item) {
+                        return strlen($item) > 0;
+                    });
 
-                $lineNumbers = [];
-                for ($i = 0; $i < $lines; $i++) {
-                    //dump($i + 0);
-                    $lineNumbers[] = $i + 0;
+                    if (count($lines)) {
+                        for ($i = 0; $i < count($lines); $i++) {
+                            $lineNumbers[] = (int) $lines[$i];
+                        }
+                    }
                 }
-                //dump($lineNumbers);
 
-                $changedFilesList[$file] = $lineNumbers;
+                if (count($lineNumbers)) {
+                    $changedFilesList[$file] = $lineNumbers;
+                }
             }
         }
         dump($changedFilesList);
 
-        die();
-        //die;
+        //$changedLineNumbers = "cd %s && git blame -p %s -- %s | grep %s";
 
-		$changedLineNumbers = "cd %s && git blame -p %s -- %s | grep %s";
+        $filesCollection = [];
+        foreach ($changedFilesList as $filePath => $changedLineNumbers) {
+            if (file_exists($filePath) === false) {
+                continue; //skip deleted
+            }
 
-		$filesCollection = [];
-		foreach ($changedFilesList as $filePath) {
-			if (file_exists($filePath) === false) {
-				continue; //skip deleted
-			}
+            $file = new File($filePath);
 
-			$file = new File($filePath);
+            //$xx = sprintf($changedLineNumbers, $this->basePath, $commitSHA, $filePath, $commitSHA);
 
-			$xx = sprintf($changedLineNumbers, $this->basePath, $commitSHA, $filePath, $commitSHA);
+            dump($changedLineNumbers);
+            foreach ($changedLineNumbers as $row) {
+                preg_match("#^[a-z0-9]{40}\s(\d+)\s(\d+)#", $row, $match);
+                dump($match);
 
-			foreach (explode("\n", `$xx`) as $row) {
-				preg_match("#^[a-z0-9]{40}\s(\d+)\s(\d+)#", $row, $match);
+                if (isset($match[1])) {
+                    $file->markLineAsChanged($match[1]);
+                }
 
-				if (isset($match[1])) {
-					$file->markLineAsChanged($match[1]);
-				}
+                if (isset($match[2])) {
+                    $file->markLineAsChanged($match[2]);
+                }
+            }
 
-				if (isset($match[2])) {
-					$file->markLineAsChanged($match[2]);
-				}
-			}
+            $filesCollection[] = $file;
+        }
+        dump($filesCollection);
+        die;
 
-			$filesCollection[] = $file;
-		}
-
-		return $filesCollection;
-	}
+        return $filesCollection;
+    }
 
 }
--- src/File.php
+++ src/File.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Ayeo\Sniffer;
 
 class File
